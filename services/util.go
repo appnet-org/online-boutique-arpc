06f3aa85046bf55a6bd028354d87afb5eab13694
Zhuo Chen <zhuochen0624@gmail.com>
2025-11-25 12:20:08 -0500

    refactor: update image references to use arpc-tcp and change default log level to info

--- services/util.go
+++ services/util.go
@@ -17,7 +17,7 @@ import (
 func getLoggingConfig() *logging.Config {
 	level := os.Getenv("LOG_LEVEL")
 	if level == "" {
-		level = "debug"
+		level = "info"
 	}
 
 	format := os.Getenv("LOG_FORMAT")
